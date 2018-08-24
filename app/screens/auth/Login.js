5c181eb2114ee393df1ce293ad01b9ed636b44ea
Xande <a.lage@jjcdobrasil.com.br>
2018-08-24 18:10:50 -0300

    Melhoria no login

--- app/screens/auth/Login.js
+++ app/screens/auth/Login.js
@@ -1,65 +1,83 @@
 import React from "react";
-import { View, TextInput, Text } from "react-native";
+import { ScrollView, View, TextInput, Text } from "react-native";
 import { Button } from "native-base";
 export default class LoginScreen extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      email: "",
-      pass: ""
+      email: "suzan@hada.digital",
+      pass: "6962"
     };
   }
 
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: "white" }}>
+      <View style={{ flex: 1 }}>
+        <ScrollView style={{ flex: 1, backgroundColor: "white", top: 80 }}>
+          <View style={{ flex: 1, justifyContent: "center", margin: 10 }}>
+            <View
+              style={{
+                alignSelf: "center",
+                backgroundColor: "green",
+                width: 100,
+                height: 100,
+                borderRadius: 10,
+                marginBottom: 40
+              }}
+            />
+            <TextInput
+              style={{
+                height: 40,
+                padding: 10,
+                borderColor: "green",
+                borderWidth: 1,
+                borderRadius: 10,
+                margin: 5,
+                textAlign: "center"
+              }}
+              placeholder="E-mail"
+              onChangeText={email => this.setState({ email })}
+              value={this.state.email}
+            />
+            <TextInput
+              secureTextEntry={true}
+              style={{
+                height: 40,
+                padding: 10,
+                borderColor: "green",
+                borderWidth: 1,
+                borderRadius: 10,
+                margin: 5,
+                textAlign: "center"
+              }}
+              placeholder="Senha"
+              onChangeText={pass => this.setState({ pass })}
+              value={this.state.pass}
+            />
+            <Button
+              style={{
+                alignSelf: "center",
+                paddingHorizontal: 30,
+                margin: 5,
+                borderRadius: 25
+              }}
+              success
+            >
+              <Text>ENTRAR</Text>
+            </Button>
 
-
-        <View style={{ flex: 1, justifyContent: "center", margin: 10 }}>
-        <View
-          style={{
-            alignSelf: "center",
-            backgroundColor: "blue",
-            width: 100,
-            height: 100,
-            borderRadius: 10,
-            marginBottom: 40
-          }}
-        />
-          <TextInput
-            style={{
-              height: 40,
-              padding: 10,
-              borderColor: "gray",
-              borderWidth: 1,
-              borderRadius: 10,
-              margin: 5
-            }}
-            placeholder="E-mail"
-            onChangeText={email => this.setState({ email })}
-            value={this.state.email}
-          />
-          <TextInput
-            style={{
-              height: 40,
-              padding: 10,
-              borderColor: "gray",
-              borderWidth: 1,
-              borderRadius: 10,
-              margin: 5
-            }}
-            placeholder="Senha"
-            onChangeText={pass => this.setState({ pass })}
-            value={this.state.pass}
-          />
-          <Button
-            style={{ alignSelf: "center", paddingHorizontal: 10, margin: 5 }}
-            success
-          >
-            <Text>ENTRAR</Text>
-          </Button>
-        </View>
+            <Text style={{ alignSelf: "center", marginTop: 40 }}>
+              Recuperar senha
+            </Text>
+          </View>
+        </ScrollView>
+        <Button
+          style={{ alignSelf: "stretch", justifyContent: 'center', paddingHorizontal: 30, borderRadius: 0}}
+          success
+        >
+          <Text style={{ textAlign: 'center' }}>CADASTRE-SE</Text>
+        </Button>
       </View>
     );
   }
